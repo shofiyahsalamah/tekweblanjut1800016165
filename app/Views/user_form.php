89c098aa5dffdc188386a21c303a9fd43a938d9e
shofiyahsalamah <shofiyah1800016165@webmail.uad.ac.id>
2020-07-08 23:18:28 +0700

    second commit

---
+++ app/Views/user_form.php
@@ -0,0 +1,101 @@
+<!doctype html>
+<html lang="en">
+<nav class="navbar navbar-expand-lg navbar-blue bg-dark">
+  <div class="container">
+    <a class="navbar-brand font-arial-bold" href="#">Shofiyah Salamah (1800016165)<i class="fas fa-book-open ml-2"
+        style="color: #4e5052;"></i></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+      aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav ml-auto"> 
+      </div>
+    </div>
+  </div>
+</nav>
+
+<div class="container mt-3">
+  <app-buku></app-buku>
+</div>
+
+
+<head>
+   <!-- Required meta tags -->
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+   <!-- Bootstrap CSS -->
+   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+
+   <title>Login Form</title>
+</head>
+
+<body>
+<div id="app">
+    <section class="section">
+      <div class="container mt-5">
+        <div class="row">
+          <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
+            <div class="login-brand">
+              <img src="<?= base_url('template') ?>/assets/img/user.png" alt="logo" width="120" 
+              style="display:block;margin:auto"/>
+          
+ 
+
+            <div class="card card-primary">
+              <div class="card-header"><h4>Login</h4></div>
+              <?php if (!empty(session()->getFlashdata('message'))): ?>
+                  <div class="alert alert-info">
+                       <?= session()->getFlashdata('message') ?>
+                  </div>
+              <?php endif; ?>
+
+            <div class="card-body">
+            <form method="POST" action="<?= base_url('login/login_action'); ?>">
+               <div class="form-group">
+                  <label for="email">Email </label>
+                  <input id="email" type="email" class="form-control" name="email" tabindex="1" required autofocus>
+                    <div class="invalid-feedback">
+                      Please fill in your email
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <div class="d-block">
+                      <label for="password" class="control-label">Password</label>
+                      <div class="float-right">
+                      </div>
+                    </div>
+                    <input id="password" type="password" class="form-control" name="password" tabindex="2" required>
+                    <div class="invalid-feedback">
+                      please fill in your password
+                    </div>
+                  </div>
+               </div>
+                <div class="form-group">
+                    <button type="submit" class="btn btn-primary btn-lg btn-block" tabindex="4">
+                      Login
+                    </button>
+                  </div>              
+               
+            </form>
+            <p>
+               <?php if (!empty(session()->getFlashdata('gagal'))) { ?>
+                  <div class="alert alert-warning">
+                     <?php echo session()->getFlashdata('gagal') ?>
+                  </div>
+               <?php } ?>
+            </p>
+         </div>
+      </div>
+   </div>
+
+   <!-- Optional JavaScript -->
+   <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+</body>
+
+</html>
